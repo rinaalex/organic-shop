960f8f991c14c8735604e2df0c5a024fdb4a191b
Katherine Burova <katherineburova@gmail.com>
2021-01-30 21:25:43 +0300

    categories list added

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from 'src/app/category.service';
 
 @Component({
   selector: 'app-product-form',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-form.component.scss']
 })
 export class ProductFormComponent implements OnInit {
+  categories$;
 
-  constructor() { }
+  constructor(categoryService: CategoryService) { 
+    this.categories$ = categoryService.getCategories();
+  }
 
   ngOnInit(): void {
   }
