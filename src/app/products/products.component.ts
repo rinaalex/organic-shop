76fbc9c587a73e39194d35514676a36ca10d08de
Katherine Burova <katherineburova@gmail.com>
2021-01-31 20:24:44 +0300

    displaying all products added

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products$;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(productService: ProductService) { 
+    this.products$ = productService.getAll();
   }
+
 
 }
