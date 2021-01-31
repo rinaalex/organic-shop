457570738bcc7db709a979b5ea830d398c76d980
Katherine Burova <katherineburova@gmail.com>
2021-01-31 18:44:11 +0300

    pagination for products added

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -13,9 +13,17 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   filteredProducts: Product[];
   subscription: Subscription;
 
+  page = 1;
+  pageSize = 5;
+  collectionSize;
+
   constructor(private productService: ProductService) {
     this.subscription = this.productService.getAll().subscribe(
-      products => this.filteredProducts = this.products = products
+      products => {
+        this.filteredProducts = this.products = products;
+        this.collectionSize = this.filteredProducts.length;
+        this.refreshProducts();
+      }
     );
   }
 
@@ -23,6 +31,12 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
     this.filteredProducts = (query) ? 
       this.products.filter(p => p.title.toLowerCase().includes(query.toLocaleLowerCase())) : 
       this.products;
+  }
+
+  refreshProducts() {
+    this.filteredProducts = this.products
+      .map((product, i) => ({id: i + 1, ...product}))
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
   }
 
   ngOnInit(): void {
