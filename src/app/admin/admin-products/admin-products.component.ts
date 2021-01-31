45bd9ec87cf6c72f3ac06c65fa40779122379e05
Katherine Burova <katherineburova@gmail.com>
2021-01-31 12:55:23 +0300

    filtering products added

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ProductService } from 'src/app/product.service';
 
 @Component({
@@ -6,14 +7,28 @@ import { ProductService } from 'src/app/product.service';
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.scss']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: any[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll().subscribe(
+      products => this.filteredProducts = this.products = products
+    );
+  }
+
+  filter(query: string) {
+    this.filteredProducts = (query) ? 
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLocaleLowerCase())) : 
+      this.products;
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
